57fab283a8d8b50138b8f98636ce0cb41e93bca3
LUKASZ\lukasz <lukasz.birski@gmail.com>
2022-02-21 16:43:40 +0100

    Compose setup created

--- app/src/main/java/pl/birski/falldetector/presentation/fragment/GraphFragment.kt
+++ app/src/main/java/pl/birski/falldetector/presentation/fragment/GraphFragment.kt
@@ -1,9 +1,39 @@
 package pl.birski.falldetector.presentation.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
-import pl.birski.falldetector.R
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
 
 @AndroidEntryPoint
-class GraphFragment : Fragment(R.layout.fragment_graph) {
+class GraphFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Greeting("Graph")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Greeting("Graph")
 }
