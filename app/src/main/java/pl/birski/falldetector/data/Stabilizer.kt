31984513e6f173d64d00bbeeeb99eab9ad7d1492
LUKASZ\lukasz <lukasz.birski@gmail.com>
2022-03-10 22:53:24 +0100

    Changed "ante" variable into nextAcceleration

--- app/src/main/java/pl/birski/falldetector/data/Stabilizer.kt
+++ app/src/main/java/pl/birski/falldetector/data/Stabilizer.kt
@@ -8,52 +8,46 @@ class Stabilizer {
 
     private val buffers: Buffers = Buffers(Constants.BUFFER_COUNT, Constants.N, 0, Double.NaN)
 
-    var anteX: Double = Double.NaN
-    var anteY: Double = Double.NaN
-    var anteZ: Double = Double.NaN
-    var anteTime: Long = 0
-    var regular: Long = 0
+    private var nextAcceleration: Acceleration = Acceleration(Double.NaN, Double.NaN, Double.NaN, 0)
+    private var regular: Long = 0
 
     fun stabilizeSignal(acceleration: Acceleration) {
         synchronized(buffers) {
             resample(acceleration = acceleration)
         }
-        anteTime = acceleration.timeStamp
-        anteX = acceleration.x
-        anteY = acceleration.y
-        anteZ = acceleration.z
+        nextAcceleration = acceleration
     }
 
     // Android sampling is irregular, hence signal is (linearly) resampled at 50 Hz
     private fun resample(acceleration: Acceleration) {
-        if (0L == anteTime) {
+        if (0L == nextAcceleration.timeStamp) {
             regular = acceleration.timeStamp + Constants.INTERVAL_MILISEC
             return
         }
         while (regular < acceleration.timeStamp) {
             val x = linearRecalculation(
-                anteTime,
-                anteX,
+                nextAcceleration.timeStamp,
+                nextAcceleration.x,
                 acceleration.timeStamp,
                 acceleration.x,
                 regular
             )
             val y = linearRecalculation(
-                anteTime,
-                anteY,
+                nextAcceleration.timeStamp,
+                nextAcceleration.y,
                 acceleration.timeStamp,
                 acceleration.x,
                 regular
             )
             val z = linearRecalculation(
-                anteTime,
-                anteZ,
+                nextAcceleration.timeStamp,
+                nextAcceleration.z,
                 acceleration.timeStamp,
                 acceleration.x,
                 regular
             )
 
-            Log.d("testuje", "resample: anteX $anteX")
+            Log.d("testuje", "resample: anteX ${nextAcceleration.x}")
             Log.d("testuje", "resample: postX ${acceleration.x}")
             Log.d("testuje", "resample: x $x")
             Log.d("testuje", "resample: --------------------------")
