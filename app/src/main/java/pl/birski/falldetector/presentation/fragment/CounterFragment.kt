299242e70f3160a8027b8347ad0174f7034f96ca
LUKASZ\lukasz <lukasz.birski@gmail.com>
2022-04-07 19:54:10 +0200

    Added counter for CounterFragment

--- app/src/main/java/pl/birski/falldetector/presentation/fragment/CounterFragment.kt
+++ app/src/main/java/pl/birski/falldetector/presentation/fragment/CounterFragment.kt
@@ -1,6 +1,7 @@
 package pl.birski.falldetector.presentation.fragment
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,6 +17,11 @@ class CounterFragment : Fragment() {
     private var _binding: FragmentCounterBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var timer: CountDownTimer
+
+    private var timerLengthSeconds = 120L
+    private var secondsRemaining = 120L
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,7 +29,12 @@ class CounterFragment : Fragment() {
     ): View? {
         _binding = FragmentCounterBinding.inflate(inflater, container, false)
 
+        startTimer()
+
         binding.counterFragmentButton.setOnClickListener {
+            timer.cancel()
+            onTimerFinished()
+
             NavHostFragment.findNavController(this)
                 .navigate(R.id.action_counterFragment_to_graphFragment)
         }
@@ -34,5 +45,46 @@ class CounterFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initTimer()
+    }
+
+    private fun startTimer() {
+
+        timer = object : CountDownTimer(secondsRemaining * 1000, 1000) {
+
+            override fun onFinish() = onTimerFinished()
+
+            override fun onTick(millisUntilFinished: Long) {
+                secondsRemaining = millisUntilFinished / 1000
+                updateCountdownView()
+            }
+        }.start()
+    }
+
+    private fun initTimer() {
+        setTimerLength()
+        updateCountdownView()
+    }
+
+    private fun onTimerFinished() {
+        binding.progressCountDown.progress = 0
+        updateCountdownView()
+    }
+
+    private fun updateCountdownView() {
+        val minutesUntilFinished = secondsRemaining / 60
+        val secondsInMinuteUntilFinished = secondsRemaining - minutesUntilFinished * 60
+        val secondsStr = secondsInMinuteUntilFinished.toString()
+        binding.countdownTextView.text =
+            "$minutesUntilFinished:${if (secondsStr.length == 2) secondsStr else "0 $secondsStr"}"
+        binding.progressCountDown.progress = (timerLengthSeconds - secondsRemaining).toInt()
+    }
+
+    private fun setTimerLength() {
+        binding.progressCountDown.max = timerLengthSeconds.toInt()
     }
 }
