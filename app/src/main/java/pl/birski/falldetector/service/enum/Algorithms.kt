0590dbec9d04e7a4b617588247f1f1059fa5ac0e
LUKASZ\lukasz <lukasz.birski@gmail.com>
2022-04-10 22:43:53 +0200

    added additional enum for algoritm selection

--- app/src/main/java/pl/birski/falldetector/service/enum/Algorithms.kt
+++ app/src/main/java/pl/birski/falldetector/service/enum/Algorithms.kt
@@ -2,7 +2,8 @@ package pl.birski.falldetector.service.enum
 
 enum class Algorithms(val number: String) {
     IMPACT_POSTURE("1"),
-    START_IMPACT_POSTURE("2");
+    START_IMPACT_POSTURE("2"),
+    START_VELOCITY_IMPACT_POSTURE("3");
 
     companion object {
         fun getByValue(number: String) = values().find { it.number == number }
