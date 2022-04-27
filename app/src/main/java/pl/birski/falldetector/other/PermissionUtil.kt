cb180ff7e78fba1704f71a2307643422fc489f90
LUKASZ\lukasz <lukasz.birski@gmail.com>
2022-04-27 22:59:46 +0200

    Added permissions and library

--- app/src/main/java/pl/birski/falldetector/other/PermissionUtil.kt
+++ app/src/main/java/pl/birski/falldetector/other/PermissionUtil.kt
@@ -2,16 +2,42 @@ package pl.birski.falldetector.other
 
 import android.Manifest
 import android.content.Context
+import android.os.Build
 import pub.devrel.easypermissions.EasyPermissions
 
 object PermissionUtil {
 
-    fun hasMessagesPermission(context: Context) = EasyPermissions.hasPermissions(
-        context,
-        Manifest.permission.SEND_SMS
-    )
+    fun hasMessagesPermission(context: Context) =
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.SEND_SMS,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.SEND_SMS,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
 
-    fun returnPermissionsArray() = arrayOf(
-        Manifest.permission.SEND_SMS
-    )
+    fun returnPermissionsArray() =
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            arrayOf(
+                Manifest.permission.SEND_SMS,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            arrayOf(
+                Manifest.permission.SEND_SMS,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
 }
