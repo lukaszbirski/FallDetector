135d96e11aab6ac9b71d773537dcd2fc4d2bca97
Lukasz Birski <lukasz.birski@gmail.com>
2022-06-13 00:31:19 +0200

    Moved frequency values to Constants

--- app/src/main/java/pl/birski/falldetector/other/Constants.kt
+++ app/src/main/java/pl/birski/falldetector/other/Constants.kt
@@ -6,7 +6,11 @@ object Constants {
     const val NOTIFICATION_CHANNEL_NAME = "Tracking"
     const val NOTIFICATION_ID = 1
 
-    const val INTERVAL_MILISEC = 20 // frequency is set to 50 Hz
+    // signal frequency is 50Hz and cut-off frequency is 0.25 Hz
+    const val FREQUENCY_CUT_OFF = 0.25
+    const val FREQUENCY_MEASUREMENT = 50.0
+
+    const val INTERVAL_MILISEC = 1_000 / FREQUENCY_MEASUREMENT
 
     private const val MIN_MAX_SLIDING_WINDOW_TIME_SEC = 0.1F // SW size for SVminmax is 0.1 s
     private const val POSTURE_DETECTION_SW_TIME_SEC = 0.4F // SW size for posture detection is 0.4 s
@@ -14,8 +18,10 @@ object Constants {
     const val MIN_MAX_SW_SIZE = MIN_MAX_SLIDING_WINDOW_TIME_SEC * 1000 / INTERVAL_MILISEC
     const val POSTURE_DETECTION_SW_SIZE = POSTURE_DETECTION_SW_TIME_SEC * 1000 / INTERVAL_MILISEC
 
-    const val IMPACT_TIME_SPAN = 2000 / INTERVAL_MILISEC // after impact need to wait 2 sec
-    const val FALLING_TIME_SPAN = 1000 / INTERVAL_MILISEC // impact is measured within 1 sec frame
+    // after impact need to wait 2 s
+    const val IMPACT_TIME_SPAN = (2000 / INTERVAL_MILISEC).toInt()
+    // impact is measured within 1 s frame
+    const val FALLING_TIME_SPAN = (1000 / INTERVAL_MILISEC).toInt()
 
     const val CUSTOM_FALL_DETECTED_RECEIVER = "pl.birski.falldetector.CUSTOM_INTENT"
 
